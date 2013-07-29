6efa8cd3ae200ac7e7dfbde44c0f569c1b1de74f
chicoreus <chicoreus@c5c8cb54-ba35-4de8-821a-e5459e5f045e>
2013-07-29 14:11:26 +0000

    ISSUE: MCZ BugID: 337 MCZ BugID: 339 PURPOSE: Adding support for OCI8 vendor specific database connection DESCRIPTION: Added a DBAbstraction class in connection_template.php that allows the connection to use either the PDO vendor independent abstraction, or the oracle OCI8 vendor specific database connection layer.  Added code to use configured connection type in image.php in its two queries.

--- trunk/datashot_php/connection_template.php
+++ trunk/datashot_php/connection_template.php
@@ -1,21 +1,35 @@
 <?php
+define("TYPE_PDO",0);
+define("TYPE_ORACLE",1);
+
+class DBAbstraction {
+    public $type;	
+	public $dbo_connection;
+	public $oracle_connection;
+}
 
 function datashot_connect() { 
+   $result = new DBAbstraction();
    try { 
    	    // For oracle:
    	    // Set LD_LIBRARY_PATH in environment to instant client 
    	    // Set TNS_ADMIN in environment to path to tnsnames.ora
    	    /*
-        $dbh = new PDO('oci:dbname=test', "username", "password", 
-                       array(PDO::ATTR_PERSISTENT => true));
+        $dbh = oci_pconnect("username", "password","connectname"); 
+        $result->type = TYPE_ORACLE;
+        $result->oracle_connection = $dbh;
+        $result->dbo_connection = null;
         */
         // for MariaDB:
         $dbh = new PDO('mysql:host=localhost;dbname=test', "username", "password", 
                        array(PDO::ATTR_PERSISTENT => true));
+        $result->type = TYPE_PDO;
+        $result->oracle_connection = null;
+        $result->dbo_connection = $dbh;
     } catch (PDOException $e) {
-        $dbh = FALSE;
+        $result = FALSE;
     }
-    return $dbh;
+    return $result;
 }
 
 ?>
