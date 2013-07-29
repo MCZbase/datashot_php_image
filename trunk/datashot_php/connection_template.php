f2695c0d968bddc7735572cb91c3901e0cf1eafb
chicoreus <chicoreus@c5c8cb54-ba35-4de8-821a-e5459e5f045e>
2013-07-29 14:21:26 +0000

    ISSUE: MCZ BugID: 337 MCZ BugID: 339 PURPOSE: Documentation DESCRIPTION: Added comment.

--- trunk/datashot_php/connection_template.php
+++ trunk/datashot_php/connection_template.php
@@ -8,6 +8,12 @@ class DBAbstraction {
 	public $oracle_connection;
 }
 
+/**
+ * Defined database connection for the Lepidoptera PHP web application.
+ * 
+ * @return false or a DBAbstraction object containing the connection 
+ * and type.
+ */
 function datashot_connect() { 
    $result = new DBAbstraction();
    try { 
@@ -20,7 +26,7 @@ function datashot_connect() {
         $result->oracle_connection = $dbh;
         $result->dbo_connection = null;
         */
-        // for MariaDB:
+        // for MariaDB (or any other PDO supported database):
         $dbh = new PDO('mysql:host=localhost;dbname=test', "username", "password", 
                        array(PDO::ATTR_PERSISTENT => true));
         $result->type = TYPE_PDO;
